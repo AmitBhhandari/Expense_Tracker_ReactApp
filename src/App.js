bd346c84a8bfb3f153e42e51dc620984ba9328f0
Amit Bhandari <115782156+AmitBhhandari@users.noreply.github.com>
2023-01-01 20:24:25 +0530

    adding expenses for tracking purpose

--- src/App.js
+++ src/App.js
@@ -1,47 +1,51 @@
-import React from "react";
+import React, { useContext } from "react";
 import "./App.css";
 
 import { Route, Redirect } from "react-router-dom";
-import LoginContext, { LoginContextProvider } from "./Components/Context/LoginContext";
+import LoginContext, { LoginContextProvider,} from "./Components/Context/LoginContext";
 import Welcome from "./Components/Pages/Welcome";
 import IncompleteProfile from "./Components/Pages/IncompleteProfile";
 import ForgotPassword from "./Components/Pages/ForgetPassword";
+import Expenses from "./Components/Pages/Expenses";
 
 import SignUp from "./Components/Pages/SignUp";
 import SignIn from "./Components/Pages/SignIn";
 import Header from "./Components/Layout/Header";
 
-
 function App() {
+  const loginCtx = useContext(LoginContext);
+  const isLoggedIn = loginCtx.isLoggedIn;
   return (
     <React.Fragment>
-      <LoginContextProvider>
-        <Header/>
-       { /*<Route path="*">
+      <Header />
+      {/*<Route path="*">
           <Redirect to="/signUp" />
   </Route>*/}
 
-        <Route path="/signUp">
-          <SignUp />
-        </Route>
+      <Route path="/signUp">
+        <SignUp />
+      </Route>
 
-        <Route path="/signIn">
-          <SignIn />
-        </Route>
+      <Route path="/signIn">
+        <SignIn />
+      </Route>
 
-        <Route path="/welcome">
-          <Welcome />
-        </Route>
+      <Route path="/welcome">
+        <Welcome />
+      </Route>
 
-        <Route path="/incompleteProfile">
-          <IncompleteProfile />
-        </Route>
+      <Route path="/incompleteProfile">
+        <IncompleteProfile />
+      </Route>
 
-        <Route path="/forgotPassword">
-          <ForgotPassword />
-        </Route>
+      <Route path="/forgotPassword">
+        <ForgotPassword />
+      </Route>
 
-      </LoginContextProvider>
+      <Route path="/expenses">
+        {isLoggedIn ? <Expenses /> : <Redirect to="/signIn" />}
+      </Route>
+
     </React.Fragment>
   );
 }
